b24122ac7bdd26ab50681f722f808f90a618f877
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-04-04 13:05:38 +0500

    Добавьте файлы проекта.

---
+++ UserTicketService.Test/CalculatorTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserTicketService.Test
+{
+    [TestFixture]
+    internal class CalculatorTest
+    {
+        [Test]
+        public void AddAlwaysReturnsExpectedValue() 
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Add(10, 220), Is.EqualTo(230));
+        }
+    }
+}
---
+++ UserTicketService.Test/Class1.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+using UserTicketService;
+
+namespace UserTicketService.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+               
+    }
+}
